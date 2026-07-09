a83c5e836eda358dfd51728265724f7946675a8e
idan yakobi <idan.yakobi@tikalk.com>
2026-07-09 12:37:40 +0300

    Orchastrator in progress

---
+++ lib/app/saga_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../saga_map/saga_map_screen.dart';
+
+class SagaApp extends StatelessWidget {
+  const SagaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SagaMapScreen(),
+    );
+  }
+}
---
+++ lib/saga_map/saga_map_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SagaMapScreen extends StatelessWidget {
+  const SagaMapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
